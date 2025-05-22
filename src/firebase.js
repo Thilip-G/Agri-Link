bd765b65f6f8fb676ea4f97653a54d9c2cd78385
Thilip-G <thiliptsdeveloper@gmail.com>
2025-05-22 13:13:52 +0530

    Second  commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,23 @@
+// src/firebase.js
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAYBOo9KRjZHhhqZm9rse7gphmjJ7_Mxxg",
-  authDomain: "agrilink-26c97.firebaseapp.com",
-  projectId: "agrilink-26c97",
-  storageBucket: "agrilink-26c97.firebasestorage.app",
-  messagingSenderId: "77951879851",
-  appId: "1:77951879851:web:33496be8a1231c91bdc5ee",
-  measurementId: "G-90VT3NW8J4"
+  apiKey: "AIzaSyCOyRfsJMaPLKinXVBUEs0BwWwZrdLswlg",
+  authDomain: "agrilink-b5b9e.firebaseapp.com",
+  projectId: "agrilink-b5b9e",
+  storageBucket: "agrilink-b5b9e.firebasestorage.app",
+  messagingSenderId: "179672675365",
+  appId: "1:179672675365:web:428e8943f70bdce880bbcc",
+  measurementId: "G-CE6LCCJ37N"
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
 
